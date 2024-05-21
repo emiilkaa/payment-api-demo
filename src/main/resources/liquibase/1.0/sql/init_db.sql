1a4b109491335e0d4a3703b4adbe1852a0099953
e.karachik <e.karachik@tinkoff.ru>
2024-05-21 03:23:16 +0300

    Fix code style

--- src/main/resources/liquibase/1.0/sql/init_db.sql
+++ src/main/resources/liquibase/1.0/sql/init_db.sql
@@ -1,56 +1,56 @@
 create table PAYMENT_API_APP.REQUEST
 (
-    ID               NUMBER not null
+    ID               NUMBER       not null
         constraint PAYMENT_API_APP_REQUEST_PK primary key,
     DATE_CREATED     TIMESTAMP(6) not null,
     DATE_UPDATED     TIMESTAMP(6) not null,
     REQUEST_TYPE     VARCHAR2(50) not null,
-    AMOUNT           NUMBER not null,
+    AMOUNT           NUMBER       not null,
     STATUS           VARCHAR2(50) not null,
     TERMINAL_ID      VARCHAR2(20) not null,
     MESSAGE          VARCHAR2(256),
     EXTENSION_FIELDS CLOB,
-    PAYMENT_ID       NUMBER not null
+    PAYMENT_ID       NUMBER       not null
 );
 
 create table PAYMENT_API_APP.PAYMENT
 (
-    ID               NUMBER not null
+    ID              NUMBER       not null
         constraint PAYMENT_API_APP_PAYMENT_PK primary key,
-    DATE_CREATED     TIMESTAMP(6) not null,
-    DATE_UPDATED     TIMESTAMP(6) not null,
-    AMOUNT           NUMBER not null,
-    ORIGINAL_AMOUNT           NUMBER not null,
-    STATUS           VARCHAR2(50) not null,
+    DATE_CREATED    TIMESTAMP(6) not null,
+    DATE_UPDATED    TIMESTAMP(6) not null,
+    AMOUNT          NUMBER       not null,
+    ORIGINAL_AMOUNT NUMBER       not null,
+    STATUS          VARCHAR2(50) not null,
     ADDITIONAL_DATA CLOB
 );
 
 create table PAYMENT_API_APP.CARD_DATA
 (
-    ID               NUMBER not null
+    ID              NUMBER       not null
         constraint PAYMENT_API_APP_CARD_DATA_PK primary key,
-    DATE_CREATED     TIMESTAMP(6) not null,
-    DATE_UPDATED     TIMESTAMP(6) not null,
-    PAYMENT_SCHEME           VARCHAR2(50) not null,
-    PAN           VARCHAR2(50),
-    PAN_EXP_DATE           VARCHAR2(24),
+    DATE_CREATED    TIMESTAMP(6) not null,
+    DATE_UPDATED    TIMESTAMP(6) not null,
+    PAYMENT_SCHEME  VARCHAR2(50) not null,
+    PAN             VARCHAR2(50),
+    PAN_EXP_DATE    VARCHAR2(24),
     CARDHOLDER_NAME VARCHAR2(50),
-    PAYMENT_ID       NUMBER
+    PAYMENT_ID      NUMBER
 );
 
 create table PAYMENT_API_APP.NSPK_DATA
 (
-    ID               NUMBER not null
+    ID               NUMBER       not null
         constraint PAYMENT_API_APP_NSPK_DATA_PK primary key,
     DATE_CREATED     TIMESTAMP(6) not null,
     DATE_UPDATED     TIMESTAMP(6) not null,
-    RESPONSE_CODE VARCHAR2(50),
-    ERROR_CODE VARCHAR2(30),
-    ERROR_MESSAGE VARCHAR2(50),
-    REQUEST_MESSAGE CLOB,
+    RESPONSE_CODE    VARCHAR2(50),
+    ERROR_CODE       VARCHAR2(30),
+    ERROR_MESSAGE    VARCHAR2(50),
+    REQUEST_MESSAGE  CLOB,
     RESPONSE_MESSAGE CLOB,
-    PAYMENT_ID       NUMBER not null,
-    REQUEST_ID       NUMBER not null
+    PAYMENT_ID       NUMBER       not null,
+    REQUEST_ID       NUMBER       not null
 );
 
 CREATE SEQUENCE PAYMENT_API_APP.REQUEST_SEQ
